aa7633d7e423f9ff095625797241c0e79ad2702d
baishali2502 <baishali2502@gmail.com>
2023-11-27 04:48:44 +0530

    Welcome

---
+++ Address_Book/src/Address_Book/Address_Book.java
@@ -0,0 +1,12 @@
+package Address_Book;
+import java.util.*;
+
+public class Address_Book 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Address Book Program !!");
+	}
+
+}
